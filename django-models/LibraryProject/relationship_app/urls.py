c63f0fd7d4a94d28ca3b6ffc1c6f5604f241bd0e
kaoutar <kaoutarbayassef.93@gmail.com>
2024-11-26 00:56:12 +0100

    djangomodl

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import list_books, LibraryDetailView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('relationship_app/', include('relationship_app.urls')),  # Inclure les URL de l'application
