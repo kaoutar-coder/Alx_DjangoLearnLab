d52d1496a5ec0acd56102b4af19c212c2c54cdfc
kaoutar <kaoutarbayassef.93@gmail.com>
2024-11-26 23:01:06 +0100

    sask3week10

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -41,18 +41,17 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     ROLE_CHOICES = [
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-        ('Admin', 'Member')
+
+        ('Admin'),
+        ('Librarian'),
+        ('Member'),
     ]
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
-class UserProfile(models.Model): "Admin", "Member"
 
 
