c754b86caf0f203c5f1545a46ea7b8bc042a2ec8
kaoutar <kaoutarbayassef.93@gmail.com>
2024-11-26 23:02:32 +0100

    sask3

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -40,12 +40,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    ROLE_CHOICES = [
-
-        ('Admin'),
-        ('Librarian'),
-        ('Member'),
-    ]
+    ('Admin'),
+    ('Librarian'),
+    ('Member')
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
