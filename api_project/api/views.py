68f65d12c046829865dad810e74664f018be5492
kaoutar <kaoutarbayassef.93@gmail.com>
2024-12-07 23:22:59 +0100

    newprojectdjangoo

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,8 +5,7 @@ from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
 
